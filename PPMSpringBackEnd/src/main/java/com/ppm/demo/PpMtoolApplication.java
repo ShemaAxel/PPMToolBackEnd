9e26358bbe11478ad89b112a98d2213f05fb9042
axel <shemaromeoaxel@gmail.com>
2019-10-04 15:05:09 +0200

    first commit

---
+++ PPMSpringBackEnd/src/main/java/com/ppm/demo/PpMtoolApplication.java
@@ -0,0 +1,13 @@
+package com.ppm.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpMtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpMtoolApplication.class, args);
+	}
+
+}
