e545a635c83945dea60780f40770e255ce3ef38f
axel <shemaromeoaxel@gmail.com>
2019-10-11 09:35:19 +0200

    /project CRud done.

--- ppmtool-react-client/src/reducers/projectReducer.js
+++ ppmtool-react-client/src/reducers/projectReducer.js
@@ -1,4 +1,4 @@
-import { GET_PROJECTS } from "../actions/types";
+import { GET_PROJECTS, GET_PROJECT, DELETE_PROJECT } from "../actions/types";
 
 const initialState = {
   projects: [],
@@ -7,10 +7,22 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch (action.type) {
+    case DELETE_PROJECT:
+      return {
+        ...state,
+        projects: state.projects.filter(
+          project => project.projectIdentifier !== action.payload
+        )
+      };
     case GET_PROJECTS:
       return {
         ...state,
         projects: action.payload
+      };
+    case GET_PROJECT:
+      return {
+        ...state,
+        project: action.payload
       };
     default:
       return state;
