c9ed1153488e8984e627b79f5164823cd7a3e897
axel <shemaromeoaxel@gmail.com>
2020-01-06 21:42:09 +0200

    new changes

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -8,6 +8,10 @@ import AddProject from "./components/Project/AddProject";
 import { Provider } from "react-redux";
 import store from "./store";
 import UpdateProject from "./components/Project/UpdateProject";
+import ProjectBoard from "./components/ProjectBoard/ProjectBoard";
+import AddProjectTask from "./components/ProjectBoard/ProjectTasks/AddProjectTask";
+import UpdateProjectTask from "./components/ProjectBoard/ProjectTasks/UpdateProjectTask";
+
 function App() {
   return (
     <Provider store={store}>
@@ -17,6 +21,13 @@ function App() {
           <Route exact path="/dashboard" component={Dashboard} />
           <Route exact path="/addProject" component={AddProject} />
           <Route exact path="/updateProject/:id" component={UpdateProject} />
+          <Route exact path="/projectBoard/:id" component={ProjectBoard} />
+          <Route exact path="/addProjectTask/:id" component={AddProjectTask} />
+          <Route
+            exact
+            path="/updateProjectTask/:backlog_id/:task_id"
+            component={UpdateProjectTask}
+          />
         </div>
       </Router>
     </Provider>
---
+++ ppmtool-react-client/src/actions/backlogActions.js
@@ -0,0 +1,89 @@
+import axios from "axios";
+import {
+  GET_ERRORS,
+  GET_BACKLOG,
+  GET_PROJECT_TASK,
+  DELETE_PROJECT_TASK
+} from "./types";
+
+export const addProjectTask = (
+  backlog_id,
+  projet_task,
+  history
+) => async dispatch => {
+  try {
+    await axios.post(`/api/backlog/${backlog_id}`, projet_task);
+    history.push(`/projectBoard/${backlog_id}`);
+    dispatch({
+      type: GET_ERRORS,
+      payload: {}
+    });
+  } catch (error) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: error.response.data
+    });
+  }
+};
+
+export const getBacklog = backlog_id => async dispatch => {
+  try {
+    const res = await axios.get(`/api/backlog/${backlog_id}`);
+    dispatch({
+      type: GET_BACKLOG,
+      payload: res.data
+    });
+  } catch (error) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: error.response.data
+    });
+  }
+};
+
+export const getProjectTask = (
+  backlog_id,
+  task_id,
+  history
+) => async dispatch => {
+  try {
+    const res = await axios.get(`/api/backlog/${backlog_id}/${task_id}`);
+    dispatch({
+      type: GET_PROJECT_TASK,
+      payload: res.data
+    });
+  } catch (error) {
+    history.push("/dashboard");
+  }
+};
+
+export const updateProjectTask = (
+  backlog_id,
+  task_id,
+  projet_task,
+  history
+) => async dispatch => {
+  try {
+    await axios.patch(`/api/backlog/${backlog_id}/${task_id}`, projet_task);
+    history.push(`/projectBoard/${backlog_id}`);
+    dispatch({
+      type: GET_ERRORS,
+      payload: {}
+    });
+  } catch (error) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: error.response.data
+    });
+  }
+};
+
+export const deleteProjectTask = (backlog_id, task_id) => async dispatch => {
+  if (window.confirm(`You ae deleting project task ${task_id}`)) {
+    await axios.delete(`/api/backlog/${backlog_id}/${task_id}`);
+    dispatch({
+      type: DELETE_PROJECT_TASK,
+      payload: task_id
+    });
+  }
+};
